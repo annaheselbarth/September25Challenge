8b4d68dfbf4ec688e503823c5136ece1b215346f
Anna Heselbarth <annaheselbarth@gmail.com>
2021-09-25 13:53:50 -0400

    added bronze, silver, gold commits

--- 00_Super/Program.cs
+++ 00_Super/Program.cs
@@ -10,6 +10,48 @@ namespace _00_Super
     {
         static void Main(string[] args)
         {
+
+            string word = "Supercalifragilisticexpialidocious";
+
+
+            foreach (char letter in word)
+            {
+                Console.WriteLine(letter);
+            }
+
+            Console.WriteLine();
+
+            foreach (char letter in word)
+            {
+                if (letter == 'i')
+                {
+                    Console.WriteLine(letter);
+                }
+                else if(letter == 'L')
+                {
+                    Console.WriteLine(letter);
+                }
+                else
+                {
+                    Console.WriteLine("Not an i");
+                }
+            }
+            int count = 0;
+            foreach (char letter in word)
+            {
+                count++;
+            }
+            Console.WriteLine(count);
+
+            Console.WriteLine(word.Length);
+
+        
+        
+        Console.ReadKey(); 
+            
         }
     }
 }
+    
+
+
